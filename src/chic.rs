b2e2b5919c550b683a2a3ec0dbf2bd16e62a1d37
esquivel71 <a73434@alunos.uminho.pt>
2025-10-17 12:33:03 +0100

    Try smaller authentication data

--- src/chic.rs
+++ src/chic.rs
@@ -116,10 +116,10 @@ where
     msg2[..KYBER_SYMBYTES].copy_from_slice(&keytag[KYBER_SYMBYTES..2*KYBER_SYMBYTES]);
 
     // Pre calculate initiator to responder key confirmation tag
-    hashin[2*KYBER_SYMBYTES+2*KYBER_PUBLICKEYBYTES+KYBER_CIPHERTEXTBYTES] = 1;
-    let mut aux = [0u8;2*KYBER_SYMBYTES];
-    hash_g(&mut aux, &hashin, 2*KYBER_SYMBYTES+2*KYBER_PUBLICKEYBYTES+KYBER_CIPHERTEXTBYTES+1);
-    init_tag.copy_from_slice(&aux[..KYBER_SYMBYTES]);
+    let mut to_authenticate = [0u8; 2*KYBER_SYMBYTES];
+    to_authenticate[..KYBER_SYMBYTES].copy_from_slice(key);
+    to_authenticate[KYBER_SYMBYTES..].copy_from_slice(sid);
+    hash_h(init_tag, &to_authenticate, KYBER_SYMBYTES);
 
     Ok(())
 }
@@ -210,11 +210,11 @@ where
     // If all works out
     cmov(key, &keytag, KYBER_SYMBYTES, (result&1)^1 as u8);
 
-    // Pre calculate initiator to responder key confirmation tag
-    hashin[2*KYBER_SYMBYTES+2*KYBER_PUBLICKEYBYTES+KYBER_CIPHERTEXTBYTES] = 1;
-    let mut aux = [0u8;2*KYBER_SYMBYTES];
-    hash_g(&mut aux, &hashin, 2*KYBER_SYMBYTES+2*KYBER_PUBLICKEYBYTES+KYBER_CIPHERTEXTBYTES+1);
-    init_tag.copy_from_slice(&aux[..KYBER_SYMBYTES]);
+    // Calculate initiator to responder key confirmation tag
+    let mut to_authenticate = [0u8; 2*KYBER_SYMBYTES];
+    to_authenticate[..KYBER_SYMBYTES].copy_from_slice(key);
+    to_authenticate[KYBER_SYMBYTES..].copy_from_slice(sid);
+    hash_h(init_tag, &to_authenticate, KYBER_SYMBYTES);
 
     Ok(result)
 }
